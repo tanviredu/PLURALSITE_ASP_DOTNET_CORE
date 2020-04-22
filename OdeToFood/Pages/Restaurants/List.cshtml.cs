c3f3db0d7447d106f7653a1be660bbe41ea145f3
Tanvir Rahman <ornobtanvir.git@gmail.com>
2020-04-22 20:36:06 +0600

    added

---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+
+// list.cshtml is like the view of the page
+// and List.cshtml.cs is the controller 
+// of the pages
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+
+        // make a varitable with getter and setter
+        public string Message { get; set; }
+
+        // on get process the get request
+        public void OnGet()
+        {
+            // any data this method exposes the razor page
+            // has this access to this value
+            Message = "hello World";
+        }
+    }
+}
